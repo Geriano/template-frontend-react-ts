30db58213a6ccdf6c935ecf47ffc6e27aa391f63
Geriano <gerznewbie@gmail.com>
2022-11-17 15:10:35 +0700

    disable serialization check

--- src/store.ts
+++ src/store.ts
@@ -3,6 +3,7 @@ import auth from "./Slices/auth"
 import modal from "./Slices/modal"
 import flash from "./Slices/flash"
 import login from "./Slices/login"
+import generalProfile from './Slices/profile/general'
 
 export const store = configureStore({
   reducer: {
@@ -10,7 +11,11 @@ export const store = configureStore({
     modal,
     flash,
     login,
+    generalProfile,
   },
+  middleware: middleware => middleware({
+    serializableCheck: false,
+  }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
